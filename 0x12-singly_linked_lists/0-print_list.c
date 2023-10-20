88028ca461fb8347a00af81c3e59ad86ce9b4dd1
enDior Hillary <en.dior@outlook.com>
2023-10-20 01:36:00 +0100

    Offset: 0x12 Singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - singly linked list
+ * @h: Pointer that loops through the next node
+ *
+ * Description: singly linked list node structure
+ * Return: @n onSuccess afterExec=while(loop)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef _HZ_LINKED_L
+#define _HZ_LINKED_L
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /*_HZ_LINKED_L*/
