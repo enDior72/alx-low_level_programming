4c4d5f63bedc5d1fc626af9a8e56386e5b4cadee
enDior Hillary <en.dior@outlook.com>
2023-10-20 05:15:36 +0100

    Will do->next #1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _HZ_LINKED_L
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,6 +21,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 
 #endif /*_HZ_LINKED_L*/
