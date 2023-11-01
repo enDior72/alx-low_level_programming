f8fb6dcfffb4a111289b7da2f1364e79a1025c4c
enDior Hillary <en.dior@outlook.com>
2023-11-01 05:41:43 +0000

    Offset file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	wrm = write(fd, text_content, strlen(text_content));
+	wrm = write(fd, text_content, _strlen(text_content));
 	if (wrm < 0)
 	{
 		close(fd);
