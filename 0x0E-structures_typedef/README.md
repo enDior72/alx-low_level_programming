d18089cb4f1542313246608854fb0b909a2beec3
Sam Hillary <138330223+enDior72@users.noreply.github.com>
2024-01-09 23:15:57 +0100

    Delete 0x0E-structures_typedef directory

--- 0x0E-structures_typedef/README.md
+++
@@ -1,8 +0,0 @@
-README.md file for alx-low_level_programming REPO
-
-Module: C Programming
-0x0E-structures_typedef
-
-This file is for the ALX-SE Training Programme.
-
-At ALX, We DO.HARD.THINGS!
--- 0x0E-structures_typedef/head.c
+++
@@ -1,9 +0,0 @@
-/*****************************************************************************/
-#include "main.h"
-/**
- * _strlen - returns the length of a string
- * uses _putchar()
- *
- * @s: the string
- * Return: the length of the string
- */
